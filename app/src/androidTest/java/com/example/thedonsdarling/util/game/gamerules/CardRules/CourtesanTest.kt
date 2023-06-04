ee506acfd16c4263d51f2e70d4c2cdc2c330da15
HunterKF <95839328+HunterKF@users.noreply.github.com>
2023-06-04 13:36:04 +0900

    Adding testing to Courtesan

---
+++ app/src/androidTest/java/com/example/thedonsdarling/util/game/gamerules/CardRules/CourtesanTest.kt
@@ -0,0 +1,11 @@
+package com.example.thedonsdarling.util.game.gamerules.CardRules
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class CourtesanTest {
+    @Test
+    fun test1() {
+
+    }
+}
